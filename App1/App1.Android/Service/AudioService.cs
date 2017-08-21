beebc98eb32e401119b1aab87333f385d010c718
Andrew <andrewboisen@gmail.com>
2017-08-21 20:02:26 +1000

    Fix for android audio issue

--- App1/App1.Android/Service/AudioService.cs
+++ App1/App1.Android/Service/AudioService.cs
@@ -8,14 +8,22 @@ namespace App1.Droid.Service
 {
     internal class AudioService : IAudioService
     {
+
         public void PlayAudio(string filename)
         {
             var player = new MediaPlayer();
             var fileDescriptor = Xamarin.Forms.Forms.Context.Assets.OpenFd(filename);
+
             player.Prepared += (s, e) =>
             {
                 player.Start();
             };
+
+            player.Completion += (s, e) =>
+            {
+                player.Release();
+            };
+
             player.SetDataSource(fileDescriptor.FileDescriptor, fileDescriptor.StartOffset, fileDescriptor.Length);
             player.Prepare();
         }
